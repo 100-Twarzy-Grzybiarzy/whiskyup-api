bd1d408198599f03f1e048bb972b75b2ccac9577
joannapawlowska <pawlo.joan@gmail.com>
2022-03-12 16:12:42 +0100

    feat: initial commit

---
+++ src/main/java/pl/kat/ue/whiskyup/WhiskyupApplication.java
@@ -0,0 +1,13 @@
+package pl.kat.ue.whiskyup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiskyupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kat/ue/whiskyup/WhiskyupApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kat.ue.whiskyup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
